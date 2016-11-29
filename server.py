f342637475f2c831b8fe4c076b52d7daa87ff289
nija <nija.mashruwala@gmail.com>
2016-11-28 16:24:04 -0800

    Feature: pass-through to redis

--- server.py
+++ server.py
@@ -11,8 +11,9 @@ from flask import Flask, request
 import logging
 import argparse
 import urllib2
+import redis
 
-# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 # parsing arguments
 PARSER = argparse.ArgumentParser(description='Client message processor')
@@ -22,11 +23,15 @@ PARSER.add_argument('API_base', help="the base URL for the game API")
 ARGS = PARSER.parse_args()
 
 # defining global vars
-MESSAGES = {} # A dictionary that contains message parts
+# MESSAGES = {} # A dictionary that contains message parts
 API_BASE = ARGS.API_base
 # 'https://csm45mnow5.execute-api.us-west-2.amazonaws.com/dev'
 
+# Persistent connection to redis
+redpool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+
 APP = Flask(__name__)
+
 
 # creating flask route for type argument
 @APP.route('/', methods=['GET', 'POST'])
@@ -35,57 +40,78 @@ def main_handler():
     main routing for requests
     """
     if request.method == 'POST':
-        return process_message(request.get_json())
+        # return process_message(request.get_json())
+        send_to_redis(request.get_json())
     else:
-        return get_message_stats()
+        # return get_message_stats()
+        return get_redis_stats()
 
-def get_message_stats():
-    """
-    provides a status that players can check
-    """
-    msg_count = len(MESSAGES.keys())
-    return "There are %d messages in the MESSAGES dictionary" % msg_count
 
-def process_message(msg):
-    """
-    processes the messages by combining and appending the kind code
-    """
-    msg_id = msg['Id'] # The unique ID for this message
-    part_number = msg['PartNumber'] # Which part of the message it is
-    data = msg['Data'] # The data of the message
+# def get_message_stats():
+#     """
+#     provides a status that players can check
+#     """
+#     msg_count = len(MESSAGES.keys())
+#     return "There are %d messages in the MESSAGES dictionary" % msg_count
 
-    # Try to get the parts of the message from the MESSAGES dictionary.
-    # If it's not there, create one that has None in both parts
-    parts = MESSAGES.get(msg_id, [None, None])
 
-    # store this part of the message in the correct part of the list
-    parts[part_number] = data
+def get_redis_stats():
+    reddy.redis.Redis(connection_pool=redpool)
+    redkeys = reddy.keys('/parts/*')
+    return "There are {} messages in redis".format(len(redkeys))
 
-    # store the parts in MESSAGES
-    MESSAGES[msg_id] = parts
 
-    # if both parts are filled, the message is complete
-    if None not in parts:
-        # app.logger.debug("got a complete message for %s" % msg_id)
-        print "have both parts"
-        # We can build the final message.
-        result = parts[0] + parts[1]
-        # sending the response to the score calculator
-        # format:
-        #   url -> api_base/jFgwN4GvTB1D2QiQsQ8GHwQUbbIJBS6r7ko9RVthXCJqAiobMsLRmsuwZRQTlOEW
-        #   headers -> x-gameday-token = API_token
-        #   data -> EaXA2G8cVTj1LGuRgv8ZhaGMLpJN2IKBwC5eYzAPNlJwkN4Qu1DIaI3H1zyUdf1H5NITR
-        APP.logger.debug("ID: %s" % msg_id)
-        APP.logger.debug("RESULT: %s" % result)
-        url = API_BASE + '/' + msg_id
-        print url
-        print result
-        req = urllib2.Request(url, data=result, headers={'x-gameday-token':ARGS.API_token})
-        resp = urllib2.urlopen(req)
-        resp.close()
-        print response
+def send_to_redis(msg):
+    redpath = '/parts/{}'.format(msg['Id'])
+    greenpath = '/complete'
+    reddy = redis.Redis(connection_pool=redpool)
+    length = reddy.lpush(redpath, msg)
+    if length == msg['TotalParts']:
+        length = reddy.lpush(greenpath, redpath)
+    return True
 
-    return 'OK'
+
+# def process_message(msg):
+#     """
+#     processes the messages by combining and appending the kind code
+#     """
+
+#     msg_id = msg['Id']                  # The unique ID for this message
+#     part_number = msg['PartNumber']     # Which part of the message it is
+#     data = msg['Data']                  # The data of the message
+
+#     # Try to get the parts of the message from the MESSAGES dictionary.
+#     # If it's not there, create one that has None in both parts
+#     parts = MESSAGES.get(msg_id, [None, None])
+
+#     # store this part of the message in the correct part of the list
+#     parts[part_number] = data
+
+#     # store the parts in MESSAGES
+#     MESSAGES[msg_id] = parts
+
+#     # if both parts are filled, the message is complete
+#     if None not in parts:
+#         app.logger.debug("got a complete message for %s" % msg_id)
+#         print "have both parts"
+#         # We can build the final message.
+#         result = parts[0] + parts[1]
+#         # sending the response to the score calculator
+#         # format:
+#         #   url -> api_base/jFgwN4GvTB1D2QiQsQ8GHwQUbbIJBS6r7ko9RVthXCJqAiobMsLRmsuwZRQTlOEW
+#         #   headers -> x-gameday-token = API_token
+#         #   data -> EaXA2G8cVTj1LGuRgv8ZhaGMLpJN2IKBwC5eYzAPNlJwkN4Qu1DIaI3H1zyUdf1H5NITR
+#         APP.logger.debug("ID: %s" % msg_id)
+#         APP.logger.debug("RESULT: %s" % result)
+#         url = API_BASE + '/' + msg_id
+#         print url
+#         print result
+#         req = urllib2.Request(url, data=result, headers={'x-gameday-token':ARGS.API_token})
+#         resp = urllib2.urlopen(req)
+#         resp.close()
+#         print response
+
+#     return 'OK'
 
 if __name__ == "__main__":
 
